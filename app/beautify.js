8e8fe48001c49d041ad2208bd110f0417e437273
kissrain <cjyang1123@gmail.com>
2016-11-25 17:08:39 +0800

    json美化优化

--- app/beautify.js
+++ app/beautify.js
@@ -1,27 +1,40 @@
 //
 // 格式美化
+// 用到了jsoneditor插件 https://github.com/josdejong/jsoneditor
 //
+const {clipboard} = require('electron');
 
-
-// TODO 参考  https://github.com/josdejong/jsoneditor
-
+// 美化前的jsoneditor
 var preContainer = document.getElementById("jsoneditor-pre");
 var preOptions = {
     mode: 'text'
 };
 var preEditor = new JSONEditor(preContainer, preOptions);
 
+// 美化后的jsoneditor
 var afterContainer = document.getElementById("jsoneditor-after");
 var afterOptions = {
     mode: 'tree'
 };
 var afterEditor = new JSONEditor(afterContainer, afterOptions);
 
-function jsonBeautify(rawJson) {
-    afterEditor.set(rawJson);
-}
-
-$('#button-json-beautify').click(() => {
-    jsonBeautify(preEditor.get());
+/**
+ * json美化上到下
+ */
+$('#button-json-beautify-down').click(() => {
+    afterEditor.set(preEditor.get());
 });
 
+/**
+ * json美化下到上
+ */
+$('#button-json-beautify-up').click(() => {
+    preEditor.set(afterEditor.get());
+});
+
+/**
+ * 拷贝上面json编辑器的值
+ */
+$('#button-json-copy').click(() => {
+    clipboard.writeText(afterEditor.get());
+});
